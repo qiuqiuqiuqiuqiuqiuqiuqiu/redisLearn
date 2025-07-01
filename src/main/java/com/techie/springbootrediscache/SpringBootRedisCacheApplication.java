4d4a4eb227e0afaed6a9f9484567e7c110fda74f
qiuqiuqiuqiuqiuqiuqiuqiu <yyyxxxjjjj@gmail.com>
2025-07-01 14:52:55 +0800

    learn

---
+++ src/main/java/com/techie/springbootrediscache/SpringBootRedisCacheApplication.java
@@ -0,0 +1,16 @@
+package com.techie.springbootrediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class SpringBootRedisCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisCacheApplication.class, args);
+    }
+
+}
